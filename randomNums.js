9d24f1d937d70876779b710798cc47f2fb98f0d2
Dario Passariello <dariopassariello@gmail.com>
2021-11-28 22:20:02 -0800

    dprandnums

--- randomNums.js
+++ randomNums.js
@@ -1,59 +1,68 @@
-    // CREATE AS GLOBAL FUNCTION
-    // if you use js, react or similar you not need redux
+/*!
+ * dprandnums <https://github.com/passariello/randomNumbers>
+ *
+ * Copyright (c) 2021, Dario Passariello.
+ * Licensed under the Apache-2.0 License.
+ */
 
-    let generate = window.generate = {};
-    let shuffle = window.shuffle = {};
+/***********************************************************************/
 
-    // ES6 / ECMAScript 2015
-    // Idea from https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+// CREATE AS GLOBAL FUNCTION
+  // if you use js, react or similar you not need redux
 
-    shuffle = (array) => {
-      if(!array) return null;
-      for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-      }
-      return array;
-    };
+  let generate = window.generate = {};
+  let shuffle = window.shuffle = {};
 
-    // MY FUNCTION
+  // ES6 / ECMAScript 2015
+  // Idea from https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
 
-    generate = ( total ) => { 
+  shuffle = (array) => {
+    if(!array) return null;
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  };
 
-      // CHECK
-      if( isNaN( total ) ) return null;
-      if( total < 0 ) total = 0;
-      if( total > 500000 ) total = 500000;
+  // MY FUNCTION
 
-      // INFORMATION 
-      console.clear();
-      console.group( '%cGeneral:%c',"color:orange","" );
-        console.debug( "%cCreated by:%c Dario Passariello", "color:gray", "" );
-        console.debug( "%cYou can use generate( 10000 ) here in the console to generate your random array. Also the result is cached into local storage to avoid repeating effort from client computer.", "color:gray", "" );
-        console.debug( "%c-------------------------------%c", "color:gray", "" );
-      console.groupEnd();
+  generate = ( total ) => { 
 
-      let startTime = performance.now(); // start performance measurement
+    // CHECK
+    if( isNaN( total ) ) return null;
+    if( total < 0 ) total = 0;
+    if( total > 500000 ) total = 500000;
 
-      // CACHE FLAT RESULT IN LOCAL STORAGE      
-      if( !localStorage.getItem( '_' + total ) ){
-        let array = Array.from( Array( total ).keys() );
-        localStorage.setItem( '_' + total , JSON.stringify( array ) );
-        array = undefined;
-      }
+    // INFORMATION 
+    console.clear();
+    console.group( `%c dprandnums :%c',"color:orange`,"" );
+      console.debug( "%cCreated by:%c Dario Passariello", "color:gray", "" );
+      console.debug( "%cYou can use generate( 10000 ) here in the console to generate your random array. Also the result is cached into local storage to avoid repeating effort from client computer.", "color:gray", "" );
+      console.debug( "%c-------------------------------%c", "color:gray", "" );
+    console.groupEnd();
 
-      // HANDLE THE SHUFFLE
-      let final = shuffle( JSON.parse(localStorage.getItem( '_' + total ) ) );
+    let startTime = performance.now(); // start performance measurement
 
-      let endTime = performance.now(); // stop performance measurement
+    // CACHE FLAT RESULT IN LOCAL STORAGE      
+    if( !localStorage.getItem( '_' + total ) ){
+      let array = Array.from( Array( total ).keys() );
+      localStorage.setItem( '_' + total , JSON.stringify( array ) );
+      array = undefined;
+    }
 
-      // CONSOLE THE RESULT
-      console.group( '%cResult:%c',"color:orange","" );
-        console.log( final );
-        console.debug( "%c-------------------------------%c", "color:gray", "" );
-        console.debug( total + `%c in ${(endTime-startTime).toFixed(0)} ms... it's ok for you?`, "color:gray", "");
-      console.groupEnd();
-    };
+    // HANDLE THE SHUFFLE
+    let final = shuffle( JSON.parse(localStorage.getItem( '_' + total ) ) );
 
-    // GENERATE 500,000
-    generate( 500000 );
+    let endTime = performance.now(); // stop performance measurement
+
+    // CONSOLE THE RESULT
+    console.group( '%cResult:%c',"color:orange","" );
+      console.log( final );
+      console.debug( "%c-------------------------------%c", "color:gray", "" );
+      console.debug( total + `%c in ${(endTime-startTime).toFixed(0)} ms... it's ok for you?`, "color:gray", "");
+    console.groupEnd();
+  };
+
+  // GENERATE 500,000
+  // generate( 500000 );
